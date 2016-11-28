56a00b39579fc03cf784bf5ab218645d6b092783
linus <phoenics76@gmail.com>
2016-11-28 14:09:15 +0900

    Dynamic web exercise

--- app.js
+++ app.js
@@ -6,8 +6,37 @@ var app = express();
   res.send('Hello World!');
 });*/
 //app.use(express.static(__dirname + '/public'));
+app.set("view engine", 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 console.log(__dirname);
+
+var data={count:0};
+app.get('/', function(req, res) {
+  data.count++;
+  console.log("data.count : " + data.count);
+  //console.log(req);
+  //console.log(res);
+  res.render('my_first_ejs', data);
+});
+
+app.get('/reset', function(req, res) {
+  data.count=0;
+  console.log("data.count : " + data.count);
+  res.render('my_first_ejs', data);
+});
+
+app.get('/set/count', function(req, res){
+  if(req.query.count) data.count=req.query.count;
+  console.log("data.count : " + data.count);
+  res.render('my_first_ejs', data);
+});
+
+app.get('/set/:num', function(req, res) {
+  data.count=req.params.num;
+  console.log("data.count : " + data.count);
+  res.render('my_first_ejs', data);
+});
+
 app.listen(3000, function(){
   console.log('Server On!');
 });
